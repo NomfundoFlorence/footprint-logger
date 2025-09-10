b1291b5669ea2241beca4fcf516d3a286d35de47
NomfundoFlorence <nomfundomlangeni0@gmail.com>
2025-09-10 08:21:26 +0200

    landing page & link to login & signup pages

--- src/app.js
+++ src/app.js
@@ -7,11 +7,21 @@ const { client, connectDatabase } = require("./db");
 const app = express();
 const PORT = 3000;
 
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "pug");
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("/", async (req, res) => {
   res.send("Inside the server");
+});
+
+app.get("/signup", (req, res) => {
+  res.render("signup", { title: "Sign Up" });
+});
+app.get("/login", (req, res) => {
+  res.render("login", { title: "Log In" });
 });
 
 app.post("/signup", async (req, res) => {
