2a6c76ea706631b0356de138d53d202e719cdebf
NomfundoFlorence <nomfundomlangeni0@gmail.com>
2025-09-11 23:33:24 +0200

    logger page routing & background image

--- frontend/src/components/Logger.jsx
+++ frontend/src/components/Logger.jsx
@@ -1,9 +1,10 @@
 export default function Logger() {
-  return(
+  return (
     <>
-      <h1>
-        Logger content
-      </h1>
+      <div className="absolute inset-0 h-screen bg-[url('https://images.unsplash.com/photo-1635695604201-2b718204bccb?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8A%3D%3D')] bg-cover bg-center filter blur-[4px]"></div>
+      <div className="relative flex w-screen h-screen items-center justify-center xl:justify-end xl:pr-10">
+        
+      </div>
     </>
-  )
+  );
 }
