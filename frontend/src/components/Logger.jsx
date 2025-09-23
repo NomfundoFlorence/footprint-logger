7c0ed5c25086aaceddb0ace1cacf0f2c01ae730e
NomfundoFlorence <nomfundomlangeni0@gmail.com>
2025-09-23 18:14:07 +0200

    fixed logs display

--- frontend/src/components/Logger.jsx
+++ frontend/src/components/Logger.jsx
@@ -215,10 +215,10 @@ export default function Logger() {
 
         {userLogs.length > 0 && (
           <div className="relative flex-1 bg-blue/80 p-4 rounded shadow-md w-full max-w-lg max-h-[600px] sm:h-[98%] overflow-hidden z-10">
-            <h2 className="text-xl font-bold text-green-900 mb-4 text-center">
+            <h2 className="text-xl font-bold text-green-900 mb-2 text-center">
               My Logs
             </h2>
-            <p className="text-green-700 font-semibold text-center mb-4">
+            <p className="text-green-700 font-semibold text-center mb-2">
               Total Emissions:{" "}
               {userLogs
                 .reduce((sum, log) => sum + parseFloat(log.emission || 0), 0)
@@ -226,7 +226,7 @@ export default function Logger() {
               kg CO₂
             </p>
 
-            <div className="border-y h-[500px] overflow-y-auto flex flex-col space-y-2">
+            <div className="border-y h-[480px] overflow-y-auto flex flex-col space-y-2">
               {userLogs.map((log, index) => (
                 <div
                   key={log._id}
