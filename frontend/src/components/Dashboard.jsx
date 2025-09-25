8ae14673b32b634efee9f26996ee862d7b91068b
NomfundoFlorence <nomfundomlangeni0@gmail.com>
2025-09-25 21:23:40 +0200

    form & goal visibility

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -19,8 +19,8 @@ export default function Dashboard() {
 
   const [weeklyGoal, setWeeklyGoal] = useState(null);
   const [goalStatus, setGoalStatus] = useState("");
-
-  const [latestGoal, setLatestGoal] = useState([]);
+  const [latestGoal, setLatestGoal] = useState(null);
+  const [showForm, setShowForm] = useState(false);
 
   const [loading, setLoading] = useState(false);
 
@@ -30,12 +30,9 @@ export default function Dashboard() {
     headers: { Authorization: token ? `Bearer ${token}` : null },
   };
 
-  const socket = io(BACKEND_URI, {
-    auth: {
-      token, // or wherever you store it
-    },
-  });
+  const socket = io(BACKEND_URI, { auth: { token } });
 
+  /** ------------------ Data Fetchers ------------------ **/
   function getUserLogs() {
     setActiveTab("summary");
     setLoading(true);
@@ -103,12 +100,6 @@ export default function Dashboard() {
       .finally(() => setLoading(false));
   }
 
-  function handleLogout() {
-    setTimeout(() => {
-      navigate("/login");
-    }, 1000);
-  }
-
   function getWeeklyGoals() {
     setActiveTab("weeklyGoals");
     setLoading(true);
@@ -124,6 +115,16 @@ export default function Dashboard() {
     });
   }
 
+  function fetchLatestGoal() {
+    axios
+      .get(`${BACKEND_URI}/weekly-goals`, headers)
+      .then((response) => {
+        setLatestGoal(response.data.goal);
+      })
+      .catch((err) => console.error("Failed to fetch user's goals", err));
+  }
+
+  /** ------------------ Goal Handling ------------------ **/
   function setGoal(event) {
     event.preventDefault();
     const formData = new FormData(event.target);
@@ -135,10 +136,11 @@ export default function Dashboard() {
 
     axios
       .post(`${BACKEND_URI}/weekly-goals`, data, headers)
-      .then((response) => {
+      .then(() => {
         setGoalStatus("Weekly goal set successfully!");
+        fetchLatestGoal();
+        setShowForm(false);
 
-        // disappear after 1 second
         setTimeout(() => {
           setGoalStatus("");
           event.target.reset();
@@ -150,31 +152,15 @@ export default function Dashboard() {
         } else {
           setGoalStatus("Failed to set goal.");
         }
-
-        setTimeout(() => {
-          setGoalStatus("");
-          event.target.reset();
-        }, 1200);
-
-        console.error("Failed to set goal:", err.response?.data || err);
+        setTimeout(() => setGoalStatus(""), 2000);
       });
   }
 
-  function fetchLatestGoal() {
-    axios
-      .get(`${BACKEND_URI}/weekly-goals`, headers)
-      .then((response) => {
-        // console.log(response.data.goal);
-
-        setLatestGoal(response.data.goal);
-
-        setUsersAverage([]);
-        setUserLogs([]);
-        setLeaderboard([]);
-        setWeeklyGoal(null);
-      })
-      .catch((err) => console.error("Failed to fetch user's goals", err))
-      .finally(() => setLoading(false));
+  /** ------------------ Auth ------------------ **/
+  function handleLogout() {
+    setTimeout(() => {
+      navigate("/login");
+    }, 1000);
   }
 
   useEffect(() => {
@@ -182,8 +168,10 @@ export default function Dashboard() {
     fetchLatestGoal();
   }, []);
 
+  /** ------------------ Render ------------------ **/
   return (
     <div className="flex h-screen">
+      {/* Sidebar */}
       <div className="w-16 md:w-64 max-w-64 bg-green-50 shadow-md z-10 flex flex-col">
         <div className="p-4 bg-green-700 text-white text-center md:text-left">
           <span className="hidden md:inline text-xl font-bold">
@@ -199,7 +187,7 @@ export default function Dashboard() {
               activeTab === "summary" ? "bg-green-200" : "bg-green-50"
             }`}>
             <Home className="text-green-800" />
-            <span className="ml-3 text-green-800 hover:text-green-600 hidden md:inline">
+            <span className="ml-3 hidden md:inline text-green-800">
               My summary
             </span>
           </div>
@@ -210,7 +198,7 @@ export default function Dashboard() {
               activeTab === "weeklyGoals" ? "bg-green-200" : "bg-green-50"
             }`}>
             <Goal className="text-green-800" />
-            <span className="ml-3 text-green-800 hover:text-green-600 hidden md:inline">
+            <span className="ml-3 hidden md:inline text-green-800">
               Weekly Goals
             </span>
           </div>
@@ -221,7 +209,7 @@ export default function Dashboard() {
               activeTab === "average" ? "bg-green-200" : "bg-green-50"
             }`}>
             <Users className="text-green-800" />
-            <span className="ml-3 text-green-800 hover:text-green-600 hidden md:inline">
+            <span className="ml-3 hidden md:inline text-green-800">
               Users average
             </span>
           </div>
@@ -232,7 +220,7 @@ export default function Dashboard() {
               activeTab === "leaderboard" ? "bg-green-200" : "bg-green-50"
             }`}>
             <Trophy className="text-green-800" />
-            <span className="ml-3 text-green-800 hover:text-green-600 hidden md:inline">
+            <span className="ml-3 hidden md:inline text-green-800">
               Leaderboard
             </span>
           </div>
@@ -241,7 +229,7 @@ export default function Dashboard() {
             onClick={() => navigate("/logger")}
             className="flex items-center h-12 p-4 border-t cursor-pointer hover:bg-green-100">
             <NotebookPen className="text-green-800" />
-            <span className="ml-3 text-green-800 hover:text-green-600 hidden md:inline">
+            <span className="ml-3 hidden md:inline text-green-800">
               Return to Logger
             </span>
           </div>
@@ -255,16 +243,16 @@ export default function Dashboard() {
         </button>
       </div>
 
+      {/* Main Content */}
       <div className="relative flex-1 p-4 overflow-y-auto">
-        <div
-          className="absolute inset-0 bg-[url('https://images.unsplash.com/photo-1635695604201-2b718204bccb?q=80&w=1171&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8A%3D%3D')] 
-          bg-cover bg-center filter blur-[4px] md:transform md:scale-x-[-1]"></div>
+        <div className="absolute inset-0 bg-[url('https://images.unsplash.com/photo-1635695604201-2b718204bccb?q=80&w=1171&auto=format&fit=crop')] bg-cover bg-center filter blur-[4px] md:transform md:scale-x-[-1]"></div>
 
         <div className="relative z-10">
           <h1 className="text-3xl font-bold text-green-900 mb-5">Dashboard</h1>
           {loading && <p className="text-white">Loading...</p>}
 
-          {userLogs.length > 0 && (
+          {/* ------------------ Tabs ------------------ */}
+          {activeTab === "summary" && (
             <div className="bg-white/80 p-4 rounded shadow-md">
               <h1 className="flex text-3xl font-bold text-green-900 mb-4 justify-center">
                 My Emissions
@@ -324,18 +312,7 @@ export default function Dashboard() {
             </div>
           )}
 
-          {usersAverage.length > 0 && (
-            <div className="bg-white/80 p-4 rounded shadow-md w-full max-w-xl mx-auto">
-              <h2 className="text-2xl font-bold text-green-900 mb-4 text-center">
-                Average across all users
-              </h2>
-              <p className="text-xl text-green-700 font-semibold text-center">
-                {usersAverage[0].averageEmission.toFixed(2)} kg CO₂
-              </p>
-            </div>
-          )}
-
-          {leaderboard.length > 0 && (
+          {activeTab === "leaderboard" && (
             <div className="bg-white/80 p-4 rounded shadow-md w-full max-w-xl mx-auto">
               <h2 className="text-2xl font-bold text-green-900 mb-4 text-center">
                 Low-footprint users (Top 10)
@@ -358,23 +335,39 @@ export default function Dashboard() {
             </div>
           )}
 
-          {weeklyGoal && (
-            <div>
-              {/* Weekly Tip Card */}
-              <div className="bg-white/80 p-4 rounded shadow-md w-full max-w-xl mx-auto">
-                <h2 className="text-2xl font-bold text-green-900 mb-4 text-center">
-                  Weekly Tip
-                </h2>
-                <p className="text-lg text-green-700 text-center mb-2">
-                  Your highest emission comes from <b>{weeklyGoal.category}</b>{" "}
-                  ({weeklyGoal.totalEmissions.toFixed(2)} kg CO₂).
+          {activeTab === "average" && (
+            <div className="bg-white/80 p-4 rounded shadow-md w-full max-w-xl mx-auto">
+              <h2 className="text-2xl font-bold text-green-900 mb-4 text-center">
+                Average across all users
+              </h2>
+              {usersAverage.length > 0 && (
+                <p className="text-xl text-green-700 font-semibold text-center">
+                  {usersAverage[0].averageEmission.toFixed(2)} kg CO₂
                 </p>
-                <p className="text-md text-green-800 text-center font-semibold">
-                  💡 {weeklyGoal.tip}
-                </p>
-              </div>
+              )}
+            </div>
+          )}
 
-              {/* ✅ Latest Goal Display */}
+          {activeTab === "weeklyGoals" && (
+            <div>
+              {/* Weekly Tip */}
+              {weeklyGoal && (
+                <div className="bg-white/80 p-4 rounded shadow-md w-full max-w-xl mx-auto">
+                  <h2 className="text-2xl font-bold text-green-900 mb-4 text-center">
+                    Weekly Tip
+                  </h2>
+                  <p className="text-lg text-green-700 text-center mb-2">
+                    Your highest emission comes from{" "}
+                    <b>{weeklyGoal.category}</b> (
+                    {weeklyGoal.totalEmissions.toFixed(2)} kg CO₂).
+                  </p>
+                  <p className="text-md text-green-800 text-center font-semibold">
+                    💡 {weeklyGoal.tip}
+                  </p>
+                </div>
+              )}
+
+              {/* Current Goal */}
               {latestGoal ? (
                 <div className="bg-white/80 p-4 rounded shadow-md w-full max-w-xl mx-auto mt-2">
                   <h2 className="text-2xl font-bold text-green-900 mb-4 text-center">
@@ -391,50 +384,67 @@ export default function Dashboard() {
                   </p>
                 </div>
               ) : (
-                <div className="bg-white/80 p-4 rounded shadow-md w-full max-w-xl mx-auto mt-2">
-                  <p className="text-gray-500 text-center">
-                    No weekly goal set yet.
-                  </p>
+                <div className="bg-white/80 p-4 rounded shadow-md w-full max-w-xl mx-auto mt-2 text-center">
+                  <p className="text-gray-500">No weekly goal set yet.</p>
                 </div>
               )}
 
-              {/* Form to Set Goal */}
-              <div className="bg-white/80 p-4 rounded shadow-md w-full max-w-xl mx-auto py-sm mt-2">
-                <h1 className="text-2xl font-bold text-green-900 mb-4 text-center">
-                  CO₂ Reduction Goal
-                </h1>
-                <form onSubmit={setGoal} className="flex flex-col">
-                  <label htmlFor="weekly-goal">Category:</label>
-                  <select
-                    id="weekly-category"
-                    name="weekly-category"
-                    className="bg-green-200 mb-5 h-9 hover:bg-green-300 border-b border-green-500"
-                    required>
-                    <option value="" disabled selected hidden>
-                      -- select --
-                    </option>
-                    <option value="energy-use">Energy Use</option>
-                    <option value="transportation">Transportation</option>
-                    <option value="food-consumption">Food Consumption</option>
-                    <option value="other">Other</option>
-                  </select>
-                  <label htmlFor="weekly-goal">Goal:</label>
-                  <input
-                    id="weekly-goal"
-                    name="weekly-goal"
-                    type="number"
-                    min={1}
-                    className="bg-green-200 mb-5 h-9 hover:bg-green-300 border-b border-green-500"
-                    required
-                  />
-                  {goalStatus && (
-                    <p className="text-red-500 text-center">{goalStatus}</p>
-                  )}
-                  <button className="bg-blue-500 w-2/4 ml-auto mr-auto mt-3 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded sm:w-1/3">
-                    Set Goal
-                  </button>
-                </form>
+              {/* Button to open form */}
+              <div className="w-full max-w-xl mx-auto mt-4 text-center">
+                <button
+                  onClick={() => setShowForm(!showForm)}
+                  disabled={!!latestGoal}
+                  className={`px-4 py-2 rounded font-bold ${
+                    latestGoal
+                      ? "bg-gray-400 text-white cursor-not-allowed"
+                      : "bg-blue-500 hover:bg-blue-700 text-white"
+                  }`}>
+                  {latestGoal ? "Goal already set" : "Set Weekly Goal"}
+                </button>
               </div>
+
+              {/* Goal Form */}
+              {showForm && !latestGoal && (
+                <div className="bg-white/80 p-4 rounded shadow-md w-full max-w-xl mx-auto mt-2">
+                  <h1 className="text-2xl font-bold text-green-900 mb-4 text-center">
+                    CO₂ Reduction Goal
+                  </h1>
+                  <form onSubmit={setGoal} className="flex flex-col">
+                    <label htmlFor="weekly-category">Category:</label>
+                    <select
+                      id="weekly-category"
+                      name="weekly-category"
+                      className="bg-green-200 mb-5 h-9 hover:bg-green-300 border-b border-green-500"
+                      required>
+                      <option value="" disabled selected hidden>
+                        -- select --
+                      </option>
+                      <option value="energy-use">Energy Use</option>
+                      <option value="transportation">Transportation</option>
+                      <option value="food-consumption">Food Consumption</option>
+                      <option value="other">Other</option>
+                    </select>
+
+                    <label htmlFor="weekly-goal">Goal (kg CO₂):</label>
+                    <input
+                      id="weekly-goal"
+                      name="weekly-goal"
+                      type="number"
+                      min={1}
+                      className="bg-green-200 mb-5 h-9 hover:bg-green-300 border-b border-green-500"
+                      required
+                    />
+
+                    {goalStatus && (
+                      <p className="text-red-500 text-center">{goalStatus}</p>
+                    )}
+
+                    <button className="bg-blue-500 w-2/4 mx-auto mt-3 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded sm:w-1/3">
+                      Set Goal
+                    </button>
+                  </form>
+                </div>
+              )}
             </div>
           )}
         </div>
