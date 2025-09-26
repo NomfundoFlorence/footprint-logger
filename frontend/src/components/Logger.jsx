91f681d774588e5da0240f185f012a83584d37b6
NomfundoFlorence <nomfundomlangeni0@gmail.com>
2025-09-26 03:15:46 +0200

    lint correction

--- frontend/src/components/Logger.jsx
+++ frontend/src/components/Logger.jsx
@@ -117,6 +117,7 @@ export default function Logger() {
 
   useEffect(() => {
     getUserLogs();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   function getUserLogs() {
